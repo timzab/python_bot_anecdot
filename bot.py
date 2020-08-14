930590925552c7b4322a2d11417b717d4fb81162
timzab <a_t_i@mail.ru>
2020-08-14 15:58:59 +0300

    add new anekdot files

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 import random
 
 # Подключаем модуль для Телеграма
-#PyTelegramBotAPI
+# PyTelegramBotAPI
 import telebot
 # Импортируем типы из модуля, чтобы создавать кнопки
 from telebot import types
@@ -22,6 +22,21 @@ with open("shtirlits.txt", "r", encoding="utf-8") as f:
 with open("rzhevskiy.txt", "r", encoding="utf-8") as f:
     for line in f:
         rzhevskiy = line.split('*')
+with open("vasiliy.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        vasiliy = line.split('*')
+with open("chukcha.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        chukcha = line.split('*')
+with open("vovochka.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        vovochka = line.split('*')
+with open("animals.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        animals = line.split('*')
+with open("barin.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        barin = line.split('*')
 # with open("housewife2.txt", "r", encoding="utf-8") as f:
 #     for line in f:
 #         housewife = line.split('*')
@@ -63,21 +78,25 @@ def get_text_messages(message):
 
         keyboard.add(key_rzhevskiy)
 
-        # key_housewife = types.InlineKeyboardButton(text='ЗА ХОЗЯЙКУ', callback_data='housewife')
-        #
-        # keyboard.add(key_housewife)
-        #
-        # key_housewife = types.InlineKeyboardButton(text='ЗА ЗДОРОВЬЕ', callback_data='housewife')
-        #
-        # keyboard.add(key_housewife)
-        #
-        # key_man = types.InlineKeyboardButton(text='ЗА МУЖЧИН', callback_data='man')
-        #
-        # keyboard.add(key_man)
-        #
-        # key_birthday = types.InlineKeyboardButton(text='ЗА ВИНОВНИКА ТОРЖЕСТВА', callback_data='birthday')
-        #
-        # keyboard.add(key_birthday)
+        key_vasiliy = types.InlineKeyboardButton(text='Чапаев', callback_data='vasiliy')
+
+        keyboard.add(key_vasiliy)
+
+        key_chukcha = types.InlineKeyboardButton(text='Чукча', callback_data='chukcha')
+
+        keyboard.add(key_chukcha)
+
+        key_vovochka = types.InlineKeyboardButton(text='Вовочка', callback_data='vovochka')
+
+        keyboard.add(key_vovochka)
+
+        key_animals = types.InlineKeyboardButton(text='Животный мир', callback_data='animals')
+
+        keyboard.add(key_animals)
+
+        key_barin = types.InlineKeyboardButton(text='Барин и слуга', callback_data='barin')
+
+        keyboard.add(key_barin)
 
         # Показываем все кнопки сразу и пишем сообщение о выборе
 
@@ -103,14 +122,16 @@ def callback_worker(call):
         msg = random.choice(shtirlits)
     elif call.data == "rzhevskiy":
         msg = random.choice(rzhevskiy)
-    # elif call.data == "housewife":
-    #     msg = random.choice(housewife)
-    # elif call.data == "health":
-    #     msg = random.choice(health)
-    # elif call.data == "man":
-    #     msg = random.choice(man)
-    # elif call.data == "birthday":
-    #     msg = random.choice(birthday)
+    elif call.data == "vasiliy":
+        msg = random.choice(vasiliy)
+    elif call.data == "chukcha":
+        msg = random.choice(chukcha)
+    elif call.data == "vovochka":
+        msg = random.choice(vovochka)
+    elif call.data == "animals":
+        msg = random.choice(animals)
+    elif call.data == "barin":
+        msg = random.choice(barin)
 
     # bot.delete_message(call.chat.id, call.message.message_id)
     bot.send_message(call.message.chat.id, msg)
